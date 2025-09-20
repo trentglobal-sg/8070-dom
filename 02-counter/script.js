c6c9444085ba2d964fd57f6e904386d5504304ca
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-09-20 08:00:05 +0000

    Added additional challenges

--- 02-counter/script.js
+++ 02-counter/script.js
@@ -20,3 +20,12 @@ incrementBtn.addEventListener("click", function(){
     number++;
     updateCount();
 })
+
+/* EXTRA CHALLENGES
+Counter challenges:
+1. Make sure the number cannot go above 20
+2. Make sure the Number cannot go below 0
+3. Display the number in red if odd
+4. Display the number in green if even
+
+*/
